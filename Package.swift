e8832732164047a698d34c1a92fa8d31b5c0a698
Wangruizhen <18081128945@163.com>
2026-09-18 00:25:50 +0800

    release: v1.3.5

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AvatarKit",
-            url: "https://github.com/spatius-ai/avatarkit-ios-release/releases/download/v1.3.4/AvatarKit_202608311739.zip",
-            checksum: "bbe56fc481d27da764040d77031fc11e52870a1762b2cf4b77a4ba516fc5f12c"
+            url: "https://github.com/spatius-ai/avatarkit-ios-release/releases/download/v1.3.5/AvatarKit_202609180015.zip",
+            checksum: "3ef5322b1df8afcea6ef648a090daa7919fa62b5031dd6b2d6a116eb65c9756b"
         )
     ]
 )
